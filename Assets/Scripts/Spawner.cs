d8292bf979b1d8bab0b7d5373df29e6a48c19804
Juan Herrera <juanpablozaid@gmail.com>
2020-04-03 20:14:47 -0700

    Added Photon matchmaking

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject[] Tetriminoes;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        NewTetrimino();
+    }
+
+    // Update is called once per frame
+    public void NewTetrimino()
+    {
+        Instantiate(Tetriminoes[Random.Range(0, Tetriminoes.Length)], transform.position, Quaternion.identity);
+    }
+}
